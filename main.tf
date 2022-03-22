5021a956cbfedb849a9872a1b530178f0360c783
Shir Fogel <shirfogel@cs.colman.ac.il>
2022-03-22 16:47:14 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+#A resource might be a physical component such as a server, or it can be a logical resource such as a Heroku application.
+resource "random_pet" "rg-name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg-name.id
+  location = var.resource_group_location
+}
